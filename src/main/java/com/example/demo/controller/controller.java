133cce658c3ea9d88aa2bba2a36a9d7a407b146a
BillLiuTingYu <bill.liu@hy-tech.com.tw>
2021-03-18 14:18:25 +0800

    abc

---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class controller {
+	
+	@GetMapping("/test01")
+	public String example01() {
+		return "Hello World!!";
+	}
+	
+	@GetMapping("/test01")
+	public String example02() {
+		return "GOGOGO";
+	}
+}
