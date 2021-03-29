7e65c2db632de596da5f378dcff0cd33ba05ef16
wlkbfather <80864433+wlkbfather@users.noreply.github.com>
2021-03-29 09:43:01 +0800

    Update controller.java

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class controller {
 	
+	@Value("${test01}")
+	private String test01;
+	
 	@GetMapping("/test01")
 	public String example01() {
+		System.out.println("test01");
 		return "Hello World!!";
 	}
 	
 	@GetMapping("/test02")
 	public String example02() {
-		return "GOGOGO";
+		System.out.println("test02");
+		return "GOGOGO!!!!";
 	}
+	@GetMapping("/test03")
+	public String example03() {
+		System.out.println("test03 = "+test01);
+		return "GOGOGO!!!!";
+	}
+	
 }
